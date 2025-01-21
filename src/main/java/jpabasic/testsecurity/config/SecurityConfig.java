cde4d0bcca2247d7c078d8b871774d728f639b5c
kim <youngwoo7804@gmail.com>
2025-01-21 22:26:48 +0900

    mysql db 설정

--- src/main/java/jpabasic/testsecurity/config/SecurityConfig.java
+++ src/main/java/jpabasic/testsecurity/config/SecurityConfig.java
@@ -4,11 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -19,6 +24,12 @@ public class SecurityConfig {
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated()
                 );
+        http
+                .formLogin((auth) -> auth.loginPage("/login")
+                        .loginProcessingUrl("/loginProc")
+                        .permitAll());
+        http
+                .csrf((auth) -> auth.disable());
         return http.build();
     }
 }
--- src/main/java/jpabasic/testsecurity/controller/LoginController.java
+++ src/main/java/jpabasic/testsecurity/controller/LoginController.java
@@ -1,4 +1,12 @@
 package jpabasic.testsecurity.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+    @GetMapping("login")
+    public String loginP(){
+        return "login";
+    }
 }
